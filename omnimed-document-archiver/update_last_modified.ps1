44ef4c4169579b40c9981819a1b9c8f0272ae28e
Antoine Cloutier <acloutier@omnimed.com>
2023-12-14 10:35:08 -0500

    Update lastmodified

--- omnimed-document-archiver/update_last_modified.ps1
+++ omnimed-document-archiver/update_last_modified.ps1
@@ -1,1 +1,1 @@
-Get-ChildItem -File -Recurse | ForEach-Object { $_.Name + '=' + $_.LastWriteTime.toString('yyyy.MM.dd HH:mm:ss') } | Out-File -FilePath 'lastmodified.txt' -Encoding Default
+Get-ChildItem -File -Recurse | ForEach-Object { $_.Name + '=' + $_.LastWriteTime.toString('yyyy.MM.dd HH:mm:ss') } | Out-File -FilePath 'last-modified.dat' -Encoding Default
